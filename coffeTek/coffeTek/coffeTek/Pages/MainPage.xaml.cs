b41a14411d3b2322ef83e83dcc87389ebd373d00
Fernando Lopes <fernandolopes.s@gmail.com>
2019-02-19 13:35:36 -0300

    adicionando as imagens no projeto android e renomeando algumas que tinham caracteres inválidos

---
+++ coffeTek/coffeTek/coffeTek/Pages/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using coffeTek.ViewModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Net.Http;
+using Xamarin.Forms;
+
+namespace coffeTek.Pages
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly HttpClient _httpClient = new HttpClient()
+            { BaseAddress = new Uri("https://desafio-mobility.herokuapp.com/") };
+        public ObservableCollection<ProductViewModel> Productions { get; set; }
+        private JsonSerializer _serializer = new JsonSerializer();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            
+
+            Productions = GetProducts();
+            lstView.ItemsSource = Productions;
+        }
+
+        private void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as ProductViewModel;
+            // Your code here
+        }
+
+        private ObservableCollection<ProductViewModel> GetProducts()
+        {
+            try
+            {
+                var response = _httpClient.GetAsync("products.json").Result;
+                response.EnsureSuccessStatusCode();
+
+                using (var stream = response.Content.ReadAsStreamAsync().Result)
+                using (var reader = new StreamReader(stream))
+                using (var json = new JsonTextReader(reader))
+                {
+                    return _serializer.Deserialize<ResultProducts>(json).Products;
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+    }
+}
---
+++ coffeTek/coffeTek/coffeTek/ViewModels/ProductViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+
+namespace coffeTek.ViewModels
+{
+    public class ProductViewModel : BaseViewModel
+    {
+        public string Title { get; set; }
+        public int Size { get; set; }
+        public int Sugar { get; set; }
+        public string[] Additional { get; set; }
+        private string imageBase64;
+
+        [JsonProperty("image")]
+        public string ImageBase64
+        {
+            get { return imageBase64; }
+            set
+            {
+                imageBase64 = value.Replace("data:image/png;base64,", "");
+                OnPropertyChanged("imageBase64");
+                OnPropertyChanged("Image");
+            }
+        }
+
+        private Xamarin.Forms.ImageSource image;
+        public Xamarin.Forms.ImageSource Image
+        {
+            get
+            {
+                return Xamarin.Forms.ImageSource.FromStream(
+                    () => new MemoryStream(Convert.FromBase64String(imageBase64)));
+            }
+        }
+    }
+
+    public class ResultProducts
+    {
+        public ObservableCollection<ProductViewModel> Products { get; set; }
+    }
+}

5f028792e16b87bd438ec0187ec7f427fc9616ed
Fernando Lopes <fernandolopes.s@gmail.com>
2019-02-19 13:37:39 -0300

    adicionando a tela de Splash. Foi preciso recortar a imagem de splash do img 'Screen.png'

--- coffeTek/coffeTek/coffeTek.Android/MainActivity.cs
+++ coffeTek/coffeTek/coffeTek.Android/MainActivity.cs
@@ -7,9 +7,9 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
-namespace coffeTek.Droid
+namespace desafio.Droid
 {
-    [Activity(Label = "coffeTek", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "desafio", Icon = "@mipmap/icon", Theme = "@style/MyAppTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
